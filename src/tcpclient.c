738abfab6d6ca7d97da25aac39b977429f127478
Matt Schurenko <mschurenko@demonware.net>
2016-12-22 00:31:38 -0500

    testing

--- src/tcpclient.c
+++ src/tcpclient.c
@@ -248,7 +248,7 @@ int tcpclient_connect(tcpclient_t *client, const char *host, const char *port, c
 
 	if (client->state == STATE_INIT) {
 		// Resolve address, create socket, set nonblocking, setup callbacks, fire connect
-		printf("HOST IS: %s", host);
+		stats_error_log("HOST IS: %s", host);
 		if (client->config->always_resolve_dns == true && client->addr != NULL) {
 			/*freeaddrinfo(client->addr);
 			client->addr = NULL;*/
