7fec5f4113608dee7c790f779820f6f0d169f35e
Matt Schurenko <mschurenko@demonware.net>
2016-12-21 19:39:40 -0500

    testing

--- src/tcpclient.c
+++ src/tcpclient.c
@@ -251,7 +251,7 @@ int tcpclient_connect(tcpclient_t *client, const char *host, const char *port, c
 		if (client->config->always_resolve_dns == true && client->addr != NULL) {
 			/*freeaddrinfo(client->addr);
 			client->addr = NULL;*/
-			if (getaddrinfo(host, port, &hints, &addr) != 0) {
+			if (getaddrinfo("host1", port, &hints, &addr) != 0) {
 				stats_error_log("tcpclient: Error resolving backend address %s: %s", host, gai_strerror(errno));
 				return 3;
 			}
